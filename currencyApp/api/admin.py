1aa3f9c098394c3b7c127b4a329d75ee6e4d49b1
Eltun Gasimov <gasimoweltun@gmail.com>
2024-11-10 00:42:47 +0100

    added admin.py for User model

--- currencyApp/api/admin.py
+++ currencyApp/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'username', 'email', 'first_name', 'last_name', 'phone_number', 'account_created_on', 'updated_on')
+    list_filter = ('account_created_on', 'updated_on')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'phone_number')
+    list_editable = ('phone_number',)
+    ordering = ('-account_created_on',)
+
+admin.site.register(User, UserAdmin)
